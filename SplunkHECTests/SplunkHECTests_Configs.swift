277fb8cbbeea6a7dd119760e52164122d342f09a
Rob Gus <rob.s.gustafson@gmail.com>
2022-03-02 19:13:48 -0800

    Unit test config updated

--- SplunkHECTests/SplunkHECTests_Configs.swift
+++ SplunkHECTests/SplunkHECTests_Configs.swift
@@ -8,11 +8,8 @@
 import Foundation
 
 struct SplunkHECTests_Configs {
-    //let hec_token = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
-    //let splunk_url = "https://http-inputs_mystack.splunkcloud.com"
+    let hec_token = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+    let splunk_url = "https://http-inputs_mystack.splunkcloud.com"
     
-    
-    let hec_token = "9812b2de-ecf9-4f9e-bf7e-c5ef509b0da8"
-    let splunk_url = "https://splunk.robg.us:8088"
     
 }
